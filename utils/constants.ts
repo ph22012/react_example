c030a1b40ab3b59609797cb06294e8e5c56d5a5c
Valentin Perez <ph22012@ues.edu.sv>
2023-10-03 23:16:43 -0600

    Add constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+enum Rutas {
+    Inicio = ('/'),
+    Ejemplos = '/examples',
+    Ejemplo1 = '/examples/1',
+    Ejemplo2 = '/examples/2',
+    Ejemplo3 = '/examples/3'
+}
+
+export default Rutas;
