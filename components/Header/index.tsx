5b9527bbbe74c37a27ab8375f49a0c32c20b7fc5
Valentin Perez <ph22012@ues.edu.sv>
2023-10-03 23:21:14 -0600

    Update: All Links(Header index, examples layout) now works with Rutas from constants.ts

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import Rutas from '@/utils/constants'
 
 const Header = () => {
   return (
     <div className='w-full flex flex-row justify-between p-6 bg-blue-500 text-white'>
-      <Link href={'/'} className='text-xl font-bold'>React example</Link>
+      <Link href={Rutas.Inicio} className='text-xl font-bold'>React example</Link>
       <div className='flex flex-row gap-4'>
-        <Link href={'/'}>Inicio</Link>
-        <Link href={'/examples'}>Ejemplos</Link>
+        <Link href={Rutas.Inicio}>Inicio</Link>
+        <Link href={Rutas.Ejemplos}>Ejemplos</Link>
       </div>
     </div>
   )
